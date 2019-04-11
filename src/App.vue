b6f72747ac1abc27f78a60348dcd8f13be43bc88
Faraks <faraks01@gmail.com>
2019-04-11 04:23:04 +0300

    Создал контейнер приложения

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container id="app" direction="vertical">
+  </el-container>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style lang="scss">
+<style lang='scss'>
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1500px;
+  height: 3088px;
+  & > * {
+    padding: 0;
+  }
 }
 </style>
