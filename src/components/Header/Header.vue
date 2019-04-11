0b89867a28f15c3c3f126d11dfcc8611032529e5
Faraks <faraks01@gmail.com>
2019-04-11 05:09:51 +0300

    Создал Header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-header height="110px">
+    <el-row tag="nav" type="flex" justify="center" align="middle">
+      <el-col :span="19">
+        <img src="../../assets/logo.png" alt="Logotype">
+        <NavMenu/>
+      </el-col>
+    </el-row>
+  </el-header>
+</template>
+
+<script>
+import NavMenu from "./NavMenu.vue";
+
+export default {
+  name: "Header",
+  components: {
+    NavMenu
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+.el-header {
+  width: 100%;
+
+  .el-row {
+    width: inherit;
+    height: inherit;
+  }
+
+  .el-col {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 18px 0 42px;
+    align-items: center;
+  }
+}
+</style>
+
