29c96557ead00fd575eea734dd610dcbb88929fd
Faraks <faraks01@gmail.com>
2019-04-11 04:26:49 +0300

    Добавил Header компонент

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <el-container id="app" direction="vertical">
+    <Header/>
   </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header/Header.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Header,
   }
 };
 </script>
