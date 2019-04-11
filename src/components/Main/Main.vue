efbe3f1f203b4096704607ad9958bf15eb083bef
Faraks <faraks01@gmail.com>
2019-04-11 07:37:16 +0300

    Создал Main компонент

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-main>
+  </el-main>
+</template>
+
+<script>
+import Billboard from "./Billboard.vue";
+
+export default {
+  name: "Main",
+  components: {
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
+
